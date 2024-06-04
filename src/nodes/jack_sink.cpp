8673bbf87c07b20ecadaa7a9243d40f8b5c1ac66
Teodor Wozniak <teodor.wozniak@amagi.com>
2024-06-04 15:15:10 +0200

    use RingBuffer in jack_sink

--- src/nodes/jack_sink.cpp
+++ src/nodes/jack_sink.cpp
@@ -2,16 +2,23 @@
 #include <jack/jack.h>
 #include <jack/types.h>
 #include <string>
+extern "C" {
+#include <libavutil/samplefmt.h>
+}
+#include "../RingBuffer.hpp"
 
 class JackSink : public NodeSingleInput<av::AudioSamples> {
 
 protected:
-  int channels_count;
-  std::string port_basename;
-  std::string client_name;
+  size_t ring_buffer_size_ = 3072;
+  int channels_count_ = 2;
+  std::string port_basename_;
+  std::string client_name_;
 
-  jack_client_t *jack_client;
-  jack_port_t **jack_ports;
+  jack_client_t *jack_client_;
+  jack_port_t **jack_ports_;
+
+  std::vector<RingBuffer<float>> channel_buffers_;
 
 public:
   JackSink(const JackSink &) = delete;
@@ -21,28 +28,31 @@ public:
 
   void prepare() {
     jack_status_t status;
-    jack_client =
-        jack_client_open(client_name.c_str(), JackNoStartServer, &status);
-    if (jack_client == nullptr) {
+    jack_client_ =
+        jack_client_open(client_name_.c_str(), JackNoStartServer, &status);
+    if (jack_client_ == nullptr) {
       throw Error("unable to create jack client, status: " +
                   std::to_string(status));
     }
 
-    jack_set_process_callback(jack_client, jack_process_callback, this);
+    jack_set_process_callback(jack_client_, jack_process_callback, this);
 
-    if (jack_activate(jack_client) != 0) {
+    if (jack_activate(jack_client_) != 0) {
       throw Error("cannot activate client");
     }
 
-    jack_ports = (jack_port_t **)malloc(sizeof(jack_port_t *) * channels_count);
-    for (int i = 0; i < channels_count; i++) {
-      std::string out_port = port_basename + "_out " + std::to_string(i + 1);
+    channel_buffers_.reserve(channels_count_);
+    jack_ports_ = (jack_port_t **)malloc(sizeof(jack_port_t *) * channels_count_);
+    for (int i = 0; i < channels_count_; i++) {
+      channel_buffers_.push_back(RingBuffer<float>(ring_buffer_size_));
 
-      jack_ports[i] =
-          jack_port_register(jack_client, out_port.c_str(),
+      std::string out_port = port_basename_ + "_out " + std::to_string(i + 1);
+
+      jack_ports_[i] =
+          jack_port_register(jack_client_, out_port.c_str(),
                              JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
-      logstream << "port registered: " << jack_ports[i];
-      if (jack_ports[i] == nullptr) {
+      logstream << "port registered: " << jack_ports_[i];
+      if (jack_ports_[i] == nullptr) {
         throw Error("unable to register jack port");
       }
 
@@ -57,15 +67,15 @@ public:
   }
 
   ~JackSink() {
-    for (int i = 0; i < channels_count; i++) {
-      if (jack_ports[i]) {
-        jack_port_unregister(jack_client, jack_ports[i]);
+    for (int i = 0; i < channels_count_; i++) {
+      if (jack_ports_[i]) {
+        jack_port_unregister(jack_client_, jack_ports_[i]);
       }
     }
-    if (jack_client) {
-      jack_client_close(jack_client);
+    if (jack_client_) {
+      jack_client_close(jack_client_);
     }
-    free(jack_ports);
+    free(jack_ports_);
   }
 
   using NodeSingleInput<av::AudioSamples>::NodeSingleInput;
@@ -77,10 +87,6 @@ public:
   }
 
   virtual void process() override {
-    // do we need to do sth here?
-  }
-
-  void jack_process(jack_nframes_t nframes) {
     av::AudioSamples as = this->source_->get();
     if (!as.isComplete())
       return;
@@ -92,24 +98,41 @@ public:
       return;
     }
 
-    if (nframes != as.samplesCount()) {
-      // handle that?
+    if (as.sampleFormat().get() != AV_SAMPLE_FMT_FLTP) {
+      logstream << "audio input not compatible with jack (must be fltp)";
       return;
     }
 
-    for (int i = 0; i < as.channelsCount(); i++) {
-      if (jack_ports[i] == nullptr) {
+    for (int ch=0; ch<channels_count_; ch++) {
+      if (ch < as.channelsCount()) {
+        float* src_buffer = reinterpret_cast<float*>(as.data(ch));
+        if (!channel_buffers_[ch].writeFrom(src_buffer, src_buffer + as.samplesCount())) {
+          logstream << "ringbuffer overflow";
+        }
+      } else {
+        channel_buffers_[ch].fill(as.samplesCount(), 0);
+      }
+    }
+
+  }
+
+  void jack_process(jack_nframes_t nframes) {
+    for (int ch = 0; ch < channels_count_; ch++) {
+      if (jack_ports_[ch] == nullptr) {
         throw Error("no jack_port");
       }
 
       jack_default_audio_sample_t *buf =
           (jack_default_audio_sample_t *)jack_port_get_buffer(
-              jack_ports[i], as.samplesCount());
+              jack_ports_[ch], nframes);
       if (buf == nullptr) {
         throw Error("failed to get jack buffer");
       }
+
+      if (!channel_buffers_[ch].readTo(buf, buf + nframes)) {
+        std::fill(buf, buf + nframes, 0);
+      }
       
-      memcpy(buf, as.data() + i * as.samplesCount(), as.samplesCount()*sizeof(float));
     } 
   }
 
@@ -125,13 +148,13 @@ public:
         make_unique<EdgeSource<av::AudioSamples>>(src_edge));
 
     if (params.count("channels_count")) {
-      r->channels_count = params["channels_count"];
+      r->channels_count_ = params["channels_count"];
     }
     if (params.count("port_basename")) {
-      r->port_basename = params["port_basename"];
+      r->port_basename_ = params["port_basename"];
     }
     if (params.count("client_name")) {
-      r->client_name = params["client_name"];
+      r->client_name_ = params["client_name"];
     }
 
     r->prepare();
