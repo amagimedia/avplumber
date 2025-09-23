c0471a6d2f456a69e489f80a7ef64988a136b9cc
Teodor Wozniak <teodor.wozniak@amagi.com>
2025-09-23 14:15:27 +0200

    dmabuf with our struct from chromium

--- src/nodes/ipc_dmabuf_source.cpp
+++ src/nodes/ipc_dmabuf_source.cpp
@@ -13,41 +13,20 @@ extern "C" {
 #include <errno.h>
 #include <cstring>
 
-static constexpr int MAX_DMABUF_OBJECTS = 4;
-static constexpr int MAX_DMABUF_LAYERS = 2;
-static constexpr int MAX_DMABUF_PLANES = 4;
-
-struct DMABufPlaneDesc {
-    uint32_t object_index;
-    uint32_t offset;
-    uint32_t pitch;
+#pragma pack(push, 1)
+struct TexInfo {
+    uint32_t width;
+    uint32_t height;
+    uint32_t stride;
+    uint32_t pixel_format; // DRM FourCC or compatible value
+    uint64_t modifier;
+    uint64_t offset;
+    uint64_t timestamp; // nanoseconds
+    uint64_t frame_count;
 };
+#pragma pack(pop)
 
-struct DMABufLayerDesc {
-    uint32_t drm_format; // DRM FourCC
-    uint32_t nb_planes;
-    DMABufPlaneDesc planes[MAX_DMABUF_PLANES];
-};
-
-struct DMABufObjectDesc {
-    uint64_t size;
-    uint64_t modifier; // DRM format modifier
-};
-
-struct DMABufHeader {
-    int64_t magic_number;
-    uint32_t frame_width;
-    uint32_t frame_height;
-    struct timespec timestamp; // 1/1e9 timebase
-
-    uint32_t nb_objects;
-    DMABufObjectDesc objects[MAX_DMABUF_OBJECTS];
-
-    uint32_t nb_layers;
-    DMABufLayerDesc layers[MAX_DMABUF_LAYERS];
-};
-
-class UnixRightsReceiver {
+class UnixFdpassReceiver {
 protected:
     std::string path_;
     int sock_fd_ = -2;
@@ -56,7 +35,7 @@ protected:
     static constexpr size_t SOCK_INDEX = 0;
     static constexpr size_t EVENT_INDEX = 1;
 public:
-    UnixRightsReceiver(const std::string &path): path_(path) {
+    UnixFdpassReceiver(const std::string &path): path_(path) {
         event_fd_ = eventfd(1, 0);
         pollfds_[SOCK_INDEX].events = POLLIN;
         pollfds_[SOCK_INDEX].revents = 0;
@@ -64,7 +43,7 @@ public:
         pollfds_[EVENT_INDEX].revents = 0;
         pollfds_[EVENT_INDEX].fd = event_fd_;
     }
-    ~UnixRightsReceiver() {
+    ~UnixFdpassReceiver() {
         if (sock_fd_ >= 0) close(sock_fd_);
         if (event_fd_ >= 0) close(event_fd_);
     }
@@ -74,7 +53,7 @@ public:
     }
     bool ensureConnected() {
         if (sock_fd_ >= 0) return true;
-        int fd = socket(AF_UNIX, SOCK_SEQPACKET, 0);
+        int fd = socket(AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0);
         if (fd < 0) {
             throw Error("socket() failed for unix path " + path_);
         }
@@ -91,7 +70,7 @@ public:
             if (errno==ENOENT || errno==ECONNREFUSED) {
                 close(fd);
                 logstream << "unix socket " << path_ << " not ready to connect";
-                wallclock.sleepms(500);
+                wallclock.sleepms(200);
                 return false;
             }
             close(fd);
@@ -101,18 +80,20 @@ public:
         pollfds_[SOCK_INDEX].fd = sock_fd_;
         return true;
     }
-    bool recvHeaderAndFDs(DMABufHeader &hdr, int *fds, int fds_capacity, int &fds_received) {
+    bool recvTexInfoAndFD(TexInfo &info, int &received_fd) {
+        received_fd = -1;
         if (!ensureConnected()) return false;
-        struct msghdr msg;
+        // Prepare to receive payload + a single FD via SCM_RIGHTS
+        char control[CMSG_SPACE(sizeof(int))];
         struct iovec iov;
-        iov.iov_base = &hdr;
-        iov.iov_len = sizeof(hdr);
-        char cmsgbuf[CMSG_SPACE(sizeof(int) * MAX_DMABUF_OBJECTS)];
+        iov.iov_base = &info;
+        iov.iov_len = sizeof(info);
+        struct msghdr msg;
         memset(&msg, 0, sizeof(msg));
         msg.msg_iov = &iov;
         msg.msg_iovlen = 1;
-        msg.msg_control = cmsgbuf;
-        msg.msg_controllen = sizeof(cmsgbuf);
+        msg.msg_control = control;
+        msg.msg_controllen = sizeof(control);
         while (true) {
             int pret = poll(pollfds_, 2, -1);
             if (pret < 0) {
@@ -122,35 +103,36 @@ public:
             if (pollfds_[EVENT_INDEX].revents & POLLIN) {
                 return false;
             }
-            if (pollfds_[SOCK_INDEX].revents & POLLIN) {
+            if (pollfds_[SOCK_INDEX].revents & (POLLIN | POLLHUP | POLLERR)) {
                 break;
             }
         }
-        ssize_t r = recvmsg(sock_fd_, &msg, MSG_CMSG_CLOEXEC);
-        if (r < 0) {
-            if (errno==EAGAIN || errno==EWOULDBLOCK) return false;
-            if (errno==ECONNRESET) {
-                close(sock_fd_);
-                sock_fd_ = -1;
-                return false;
-            }
-            throw Error("recvmsg() failed on unix socket");
-        }
-        if (static_cast<size_t>(r) < sizeof(DMABufHeader)) {
-            logstream << "short recvmsg() for DMABUF header";
+        ssize_t r = recvmsg(sock_fd_, &msg, 0);
+        if (r <= 0) {
+            // connection reset or EOF: close and retry later
+            close(sock_fd_);
+            sock_fd_ = -1;
             return false;
         }
-        fds_received = 0;
+        // Ancillary data
         for (struct cmsghdr *cmsg = CMSG_FIRSTHDR(&msg);
-             cmsg != nullptr;
-             cmsg = CMSG_NXTHDR(&msg, cmsg)) {
+             cmsg != nullptr; cmsg = CMSG_NXTHDR(&msg, cmsg)) {
             if (cmsg->cmsg_level == SOL_SOCKET && cmsg->cmsg_type == SCM_RIGHTS) {
-                size_t count = (cmsg->cmsg_len - CMSG_LEN(0)) / sizeof(int);
-                int *cmsg_fds = reinterpret_cast<int*>(CMSG_DATA(cmsg));
-                for (size_t i=0; i<count && fds_received < fds_capacity; i++) {
-                    fds[fds_received++] = cmsg_fds[i];
-                }
+                int *fds = reinterpret_cast<int*>(CMSG_DATA(cmsg));
+                // Expect one fd
+                received_fd = fds[0];
+                break;
             }
+        }
+        if (received_fd < 0) {
+            logstream << "fdpass: recvmsg without FD";
+            return false;
+        }
+        if (static_cast<size_t>(r) < sizeof(TexInfo)) {
+            logstream << "fdpass: metadata too small (" << r << ")";
+            close(received_fd);
+            received_fd = -1;
+            return false;
         }
         return true;
     }
@@ -158,7 +140,7 @@ public:
 
 class IPCDMABUFSource: public NodeSingleOutput<av::VideoFrame>, public IStoppable, public ReportsFinishByFlag, public IVideoFormatSource {
 protected:
-    UnixRightsReceiver receiver_;
+    UnixFdpassReceiver receiver_;
     int width_ = 0;
     int height_ = 0;
 public:
@@ -174,53 +156,38 @@ public:
         this->finished_ = true;
     }
     virtual void process() {
-        DMABufHeader hdr;
-        int fds[MAX_DMABUF_OBJECTS] = {-1,-1,-1,-1};
-        int nfds = 0;
-        if (!receiver_.recvHeaderAndFDs(hdr, fds, MAX_DMABUF_OBJECTS, nfds)) {
-            wallclock.sleepms(10);
-            return;
-        }
-        if (hdr.magic_number != 0x12345678abcddd01) {
-            for (int i=0; i<nfds; i++) if (fds[i]>=0) close(fds[i]);
-            logstream << "invalid magic number, ignoring DMABUF packet";
-            return;
-        }
-        if (hdr.nb_objects > static_cast<uint32_t>(nfds)) {
-            for (int i=0; i<nfds; i++) if (fds[i]>=0) close(fds[i]);
-            logstream << "not enough fds received for DMABUF objects";
+        TexInfo ti{};
+        int dmabuf_fd = -1;
+        if (!receiver_.recvTexInfoAndFD(ti, dmabuf_fd)) {
+            wallclock.sleepms(5);
             return;
         }
 
         AVDRMFrameDescriptor *desc = (AVDRMFrameDescriptor*)av_mallocz(sizeof(AVDRMFrameDescriptor));
         if (!desc) {
-            for (int i=0; i<nfds; i++) if (fds[i]>=0) close(fds[i]);
+            close(dmabuf_fd);
             logstream << "av_mallocz failed for DRM descriptor";
             return;
         }
-        desc->nb_objects = hdr.nb_objects;
-        for (uint32_t i=0; i<hdr.nb_objects && i<MAX_DMABUF_OBJECTS; i++) {
-            desc->objects[i].fd = fds[i];
-            desc->objects[i].size = hdr.objects[i].size;
-            desc->objects[i].format_modifier = hdr.objects[i].modifier;
-        }
-        desc->nb_layers = hdr.nb_layers;
-        for (uint32_t l=0; l<hdr.nb_layers && l<MAX_DMABUF_LAYERS; l++) {
-            desc->layers[l].format = hdr.layers[l].drm_format;
-            desc->layers[l].nb_planes = hdr.layers[l].nb_planes;
-            for (uint32_t p=0; p<hdr.layers[l].nb_planes && p<MAX_DMABUF_PLANES; p++) {
-                desc->layers[l].planes[p].object_index = hdr.layers[l].planes[p].object_index;
-                desc->layers[l].planes[p].offset = hdr.layers[l].planes[p].offset;
-                desc->layers[l].planes[p].pitch = hdr.layers[l].planes[p].pitch;
-            }
-        }
+
+        desc->nb_objects = 1;
+        desc->objects[0].fd = dmabuf_fd;
+        desc->objects[0].size = 0; // unknown
+        desc->objects[0].format_modifier = ti.modifier;
+
+        desc->nb_layers = 1;
+        desc->layers[0].format = ti.pixel_format;
+        desc->layers[0].nb_planes = 1;
+        desc->layers[0].planes[0].object_index = 0;
+        desc->layers[0].planes[0].offset = ti.offset;
+        desc->layers[0].planes[0].pitch = ti.stride;
 
         av::VideoFrame vfrm;
         vfrm.raw()->format = AV_PIX_FMT_DRM_PRIME;
-        vfrm.raw()->width = hdr.frame_width;
-        vfrm.raw()->height = hdr.frame_height;
+        vfrm.raw()->width = ti.width;
+        vfrm.raw()->height = ti.height;
         vfrm.setTimeBase({1, 1000000});
-        vfrm.raw()->pts = hdr.timestamp.tv_sec * 1000000 + hdr.timestamp.tv_nsec / 1000;
+        vfrm.raw()->pts = ti.timestamp / 1000; // ns -> us
 
         AVBufferRef *buf = av_buffer_create(reinterpret_cast<uint8_t*>(desc), sizeof(*desc),
             [](void *opaque, uint8_t *data){
@@ -231,7 +198,7 @@ public:
                 av_free(d);
             }, desc, 0);
         if (!buf) {
-            for (int i=0; i<nfds; i++) if (fds[i]>=0) close(fds[i]);
+            close(dmabuf_fd);
             av_free(desc);
             logstream << "av_buffer_create failed for DRM descriptor";
             return;
@@ -239,8 +206,8 @@ public:
         vfrm.raw()->buf[0] = buf;
         vfrm.raw()->data[0] = reinterpret_cast<uint8_t*>(desc);
 
-        width_ = hdr.frame_width;
-        height_ = hdr.frame_height;
+        width_ = ti.width;
+        height_ = ti.height;
 
         vfrm.setComplete(true);
         this->sink_->put(vfrm);
@@ -255,5 +222,3 @@ public:
 };
 
 DECLNODE(ipc_dmabuf_source, IPCDMABUFSource);
-
-
