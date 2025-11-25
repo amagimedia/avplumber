56c2bdc3ba95064c8895e1b093149b57b395d472
Lukasz Misek <lukasz.misek@amagi.com>
2025-11-25 13:29:51 +0100

    1

--- src/nodes/force_fps.cpp
+++ src/nodes/force_fps.cpp
@@ -110,6 +110,7 @@ public:
                                 }
                                 return;
                             } else {
+                                logstream << "FFF/duplicate = " << last_frame_.pts().timestamp({1, 1000});
                                 if (!last_unused_) {
                                     // increase number of duplicated frames only if last_frame_ was already used
                                     duplicated_++;
@@ -161,6 +162,7 @@ public:
                 }
                 return;
             } else {
+                logstream << "FFF/put = " << pkt.pts().timestamp({1, 1000});
                 last_ts_ = in_ts;
                 next_ts_ = addTS(in_ts, frame_delta_);
                 setLast(pkt, false);
