11083d0312a9dc1d0b999ccd9744034f71e530c2
Jan Pietek <jan.pietek@gmail.com>
2026-01-19 10:06:03 +0100

    Revert "Merge branch 'web-ui' into develop"
    This reverts commit 3cbcdc92e27cd9d701d534b902135bf815f2e1ba, reversing
    changes made to b1065092932a243f4a36ae4b742010fae593f209.

--- tools/web-ui/frontend/src/graphStores.js
+++
@@ -1,7 +0,0 @@
-import { writable } from 'svelte/store';
-
-// Map<string, QueueStat>
-// QueueStat is the object from queues.json: {name, capacity, occupied, pps, ...}
-export const queueStatsByName = writable(new Map());
-
-
