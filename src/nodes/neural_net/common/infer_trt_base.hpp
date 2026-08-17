9bc6aed8b8e45ffa63afd1f0ae5f95dc5d004741
Jan Pietek <jan.pietek@gmail.com>
2026-08-17 23:23:39 +0200

    fix: remove duplicate TensorRT uint8 type case

--- src/nodes/neural_net/common/infer_trt_base.hpp
+++ src/nodes/neural_net/common/infer_trt_base.hpp
@@ -108,7 +108,6 @@ inline size_t elementSize(nvinfer1::DataType dt) {
     switch (dt) {
         case nvinfer1::DataType::kFLOAT: return 4;
         case nvinfer1::DataType::kHALF: return 2;
-        case nvinfer1::DataType::kUINT8: return 1;
         case nvinfer1::DataType::kINT8: return 1;
         case nvinfer1::DataType::kUINT8: return 1;
         case nvinfer1::DataType::kINT32: return 4;
